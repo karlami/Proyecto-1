a4de8bd0ba53f96ba02cd5e867900930c7a97511
fiorellaD53 <fdelgado53@estudiantec.cr>
2020-06-13 18:07:02 -0600

    Include some imports

--- CoTEC202/src/app/app.module.ts
+++ CoTEC202/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -59,7 +62,11 @@ import { WelcomeLoginComponent } from './Miscellanious/welcome-login/welcome-log
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
