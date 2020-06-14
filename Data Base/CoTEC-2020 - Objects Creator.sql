cd387feedda36397cf763cfd411b5d31eb1f202f
Rubén Salas <rubsalas@hotmail.com>
2020-06-14 07:35:22 -0600

    Creación de Data Loader Query y actualización de Objects Creator Query

--- Data Base/CoTEC-2020 - Objects Creator.sql
+++ Data Base/CoTEC-2020 - Objects Creator.sql
@@ -1,3 +1,17 @@
+/*
+--------------------------------------------------------------------
+Proyecto #1 - CoTEC-2020
+--------------------------------------------------------------------
+Instituto Técnológico de Costa Rica
+Área Académica de Ingeniería en Computadores
+Bases de Datos (CE3101) 
+I Semestre 2020
+Prof. Luis Diego Noguera Mena
+Cristhofer Azofeifa, Fiorella Delgado, Karla Rivera & Rubén Salas
+--------------------------------------------------------------------
+CoTEC-2020 - Objects Creator
+--------------------------------------------------------------------
+*/
 
 /****** Object:  Database CoTEC-2020 ******/
 CREATE DATABASE [CoTEC-2020]
@@ -9,61 +23,56 @@ GO
 
 /****** Object:  Table Ubicacion ******/
 CREATE TABLE Ubicacion(
-	idUbicacion					INT					NOT NULL,
-	continente					VARCHAR(75)			NOT NULL,
-	pais						VARCHAR(75)			NOT NULL,
-	region						VARCHAR(75)			NOT NULL
-	PRIMARY KEY(idUbicacion)
+	idUbicacion					INT	IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	continente					VARCHAR(75)								NOT NULL,
+	pais						VARCHAR(75)								NOT NULL,
+	region						VARCHAR(75)								NOT NULL
 )
 GO
 
 
 /****** Object:  Table MedidaSanitaria ******/
 CREATE TABLE MedidaSanitaria(
-	idMedidaSanitaria			INT					NOT NULL,
-	nombre						VARCHAR(100)		NOT NULL,
-	descripcion					VARCHAR(300)		NOT NULL,
-	estado						VARCHAR(20)			NOT NULL,
-	fechaInicio					DATE				NOT NULL,
-	fechaFinal					DATE				NULL,
-	PRIMARY KEY(idMedidaSanitaria)
+	idMedidaSanitaria			INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	nombre						VARCHAR(100)							NOT NULL,
+	descripcion					VARCHAR(300)							NOT NULL,
+	estado						VARCHAR(20)								NOT NULL,
+	fechaInicio					DATE									NOT NULL,
+	fechaFinal					DATE									NULL,
 )
 GO
 
 
 /****** Object:  Table Patologia ******/
 CREATE TABLE Patologia(
-	idPatologia					INT					NOT NULL,
-	nombre						VARCHAR(100)		NOT NULL,
-	descripcion					VARCHAR(300)		NOT NULL,
-	sintomas					VARCHAR(300)		NOT NULL,
-	tratamiento					VARCHAR(300)		NOT NULL,
-	PRIMARY KEY(idPatologia)
+	idPatologia					INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	nombre						VARCHAR(100)							NOT NULL,
+	descripcion					VARCHAR(300)							NOT NULL,
+	sintomas					VARCHAR(300)							NOT NULL,
+	tratamiento					VARCHAR(300)							NOT NULL,
 )
 GO
 
 
 /****** Object:  Table Medicamento ******/
 CREATE TABLE Medicamento(
-	idMedicamento				INT					NOT NULL,
-	nombre						VARCHAR(100)		NOT NULL,
-	descripcion					VARCHAR(300)		NOT NULL,
-	casaFarmaceutica			VARCHAR(100)		NOT NULL,
-	PRIMARY KEY(idMedicamento)
+	idMedicamento				INT	IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	nombre						VARCHAR(100)							NOT NULL,
+	descripcion					VARCHAR(300)							NOT NULL,
+	casaFarmaceutica			VARCHAR(100)							NOT NULL,
 )
 GO
 
 
 /****** Object:  Table Persona ******/
 CREATE TABLE Persona(
-	cedula						VARCHAR(30)			NOT NULL,
-	nombre						VARCHAR(75)			NOT NULL,
-	primerApellido				VARCHAR(75)			NOT NULL,
-	segundoApellido				VARCHAR(75)			NOT NULL,
-	nacionalidad				VARCHAR(50)			NOT NULL,
-	fechaNacimiento				DATE				NOT NULL,
-	idUbicacion					INT					NOT NULL,
-	PRIMARY KEY(cedula),
+	cedula						VARCHAR(30) PRIMARY KEY					NOT NULL,
+	nombre						VARCHAR(75)								NOT NULL,
+	primerApellido				VARCHAR(75)								NOT NULL,
+	segundoApellido				VARCHAR(75)								NOT NULL,
+	nacionalidad				VARCHAR(50)								NOT NULL,
+	fechaNacimiento				DATE									NOT NULL,
+	idUbicacion					INT										NOT NULL,
 	FOREIGN KEY(idUbicacion)						REFERENCES Ubicacion(idUbicacion)
 )
 GO
@@ -71,14 +80,14 @@ GO
 
 /****** Object:  Table CentroHospitalario ******/
 CREATE TABLE CentroHospitalario(
-	idCentroHospitalario		INT					NOT NULL,
-	nombre						VARCHAR(100)		NOT NULL,
-	capacidad					INT					NOT NULL,
-	capacidadUci				INT					NOT NULL,
-	director					VARCHAR(30)			NOT NULL,
-	contacto					VARCHAR(50)			NOT NULL,
-	idUbicacion					INT					NOT NULL,
-	PRIMARY KEY(idCentroHospitalario),
+	idCentroHospitalario		INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	nombre						VARCHAR(200)							NOT NULL,
+	capacidad					INT										NOT NULL,
+	capacidadUci				INT										NOT NULL,
+	contacto					VARCHAR(50)								NOT NULL,
+	director					VARCHAR(30)								NOT NULL,
+	idUbicacion					INT										NOT NULL,
+	FOREIGN KEY(director)							REFERENCES Persona(cedula),
 	FOREIGN KEY(idUbicacion)						REFERENCES Ubicacion(idUbicacion)
 )
 GO
@@ -86,11 +95,10 @@ GO
 
 /****** Object:  Table Contacto ******/
 CREATE TABLE Contacto(
-	idContacto					INT					NOT NULL,
-	correo						VARCHAR(100)		NOT NULL,
-	idPaciente					INT					NOT NULL,
-	cedula						VARCHAR(30)			NOT NULL,
-	PRIMARY KEY(idContacto),
+	idContacto					INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	correo						VARCHAR(100)							NOT NULL,
+	idPaciente					INT										NOT NULL,
+	cedula						VARCHAR(30)								NOT NULL,
 	FOREIGN KEY(cedula)								REFERENCES Persona(cedula)
 )
 GO
@@ -98,26 +106,33 @@ GO
 
 /****** Object:  Table Paciente ******/
 CREATE TABLE Paciente(
-	idPaciente					INT					NOT NULL,
-	estado						VARCHAR(100)		NOT NULL,
-	internado					VARCHAR(10)			NOT NULL,
-	uci							VARCHAR(10)			NOT NULL,
-	fechaIngreso				DATE				NOT NULL,
-	idCentroHospitalario		INT					NOT NULL,
-	cedula						VARCHAR(30)			NOT NULL,
-	PRIMARY KEY(idPaciente),
+	idPaciente					INT	IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	internado					VARCHAR(10)								NOT NULL,
+	uci							VARCHAR(10)								NOT NULL,
+	fechaIngreso				DATE									NOT NULL,
+	idCentroHospitalario		INT										NOT NULL,
+	cedula						VARCHAR(30)								NOT NULL,
 	FOREIGN KEY(idCentroHospitalario)				REFERENCES CentroHospitalario(idCentroHospitalario),
 	FOREIGN KEY(cedula)								REFERENCES Persona(cedula)
 )
 GO
 
 
+/****** Object:  Table EstadoPaciente ******/
+CREATE TABLE EstadoPaciente(
+	idEstadoPaciente					INT	IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	idPaciente							INT										NOT NULL,
+	estado								VARCHAR(30)								NOT NULL,
+	FOREIGN KEY(idPaciente)				REFERENCES Paciente(idPaciente),
+)
+GO
+
+
 /****** Object:  Table PersonaPatologia ******/
 CREATE TABLE PersonaPatologia(
-	idPersonaPatologia			INT					NOT NULL,
-	cedula						VARCHAR(30)			NOT NULL,
-	idPatologia					INT					NOT NULL,
-	PRIMARY KEY(idPersonaPatologia),
+	idPersonaPatologia			INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	cedula						VARCHAR(30)								NOT NULL,
+	idPatologia					INT										NOT NULL,
 	FOREIGN KEY(cedula)								REFERENCES Persona(cedula),
 	FOREIGN KEY(idPatologia)						REFERENCES Patologia(idPatologia)
 )
@@ -126,10 +141,9 @@ GO
 
 /****** Object:  Table PacienteMedicamento ******/
 CREATE TABLE PacienteMedicamento(
-	idPacienteMedicamento		INT					NOT NULL,
-	idMedicamento				INT					NOT NULL,
-	idPaciente					INT					NOT NULL,
-	PRIMARY KEY(idPacienteMedicamento),
+	idPacienteMedicamento		INT IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	idMedicamento				INT										NOT NULL,
+	idPaciente					INT										NOT NULL,
 	FOREIGN KEY(idMedicamento)						REFERENCES Medicamento(idMedicamento),
 	FOREIGN KEY(idPaciente)							REFERENCES Paciente(idPaciente)
 )
@@ -138,10 +152,9 @@ GO
 
 /****** Object:  Table UbicacionMedidaSanitaria ******/
 CREATE TABLE UbicacionMedidaSanitaria(
-	idUbicacionMedidaSanitaria	INT					NOT NULL,
-	idUbicacion					INT					NOT NULL,
-	idMedidaSanitaria			INT					NOT NULL,
-	PRIMARY KEY(idUbicacionMedidaSanitaria),
+	idUbicacionMedidaSanitaria	INT	IDENTITY (1, 1) PRIMARY KEY			NOT NULL,
+	idUbicacion					INT										NOT NULL,
+	idMedidaSanitaria			INT										NOT NULL,
 	FOREIGN KEY(idUbicacion)						REFERENCES Ubicacion(idUbicacion),
 	FOREIGN KEY(idMedidaSanitaria)					REFERENCES MedidaSanitaria(idMedidaSanitaria)
 )
