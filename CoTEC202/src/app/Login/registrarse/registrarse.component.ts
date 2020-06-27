762b02f8f1ae4cc0216b300eda92fefccf4eca79
karlami <karla.rivera24@gmail.com>
2020-06-27 04:49:41 -0600

    Create user model and form

--- CoTEC202/src/app/Login/registrarse/registrarse.component.ts
+++ CoTEC202/src/app/Login/registrarse/registrarse.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Modelos/user.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-registrarse',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrarse.component.css']
 })
 export class RegistrarseComponent implements OnInit {
+  user: User;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resetForm(); 
   }
+
+
+  resetForm(form?: NgForm) {
+    if (form != null)
+      form.reset();
+    this.user = {
+      userName: '',
+      password: '',
+      correo: '',
+      primerNombre: '',
+      apellido: ''
+    }
+  }
+
 
 }
---
+++ CoTEC202/src/app/Modelos/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+    userName: string;
+    password: string;
+    correo: string;
+    primerNombre: string;
+    apellido: string;
+}
